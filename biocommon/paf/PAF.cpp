045a8c1146a0c06bb2064dd76429ce8cf35a89ae
Yoonsung Kwon <shyapple1203@unist.ac.kr>
2025-07-25 10:49:01 +0900

    Fix: Change Ref and Qry on paf-reading .cpp

--- biocommon/paf/PAF.cpp
+++ biocommon/paf/PAF.cpp
@@ -7,8 +7,8 @@ namespace BGIQD {
         void PAF_Item::InitFromString(const std::string &line)
         {
             std::istringstream ist(line);
-            ist>>query_name>>query_len>>query_start>>query_end>>query_char;
-            ist>>target_name>>target_len>>target_start>>target_end;
+            ist>>target_name>>target_len>>target_start>>target_end>>query_char;
+            ist>>query_name>>query_len>>query_start>>query_end;
             //ist>>len_query_match>>len_target_match>>quality;
             ist>>match_len>>aligned_len >>quality;
             std::string extra ;
